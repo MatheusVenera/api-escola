a1c4d762db8cd7466ecb6c337be01750caad1354
matheus.venera <matheus.venera@dbrsa.com.br>
2022-06-08 10:54:53 -0300

    Implementado o cadastro de usuários do sistema

---
+++ src/models/User.js
@@ -0,0 +1,52 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nome não pode ficar vazio',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Este email já está cadastrado',
+        },
+        validate: {
+          isEmail: {
+            args: [3, 255],
+            msg: 'Email inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha deve ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
