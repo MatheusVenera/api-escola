a4f4a70bd96aa9f1da3165d98df87e267ae8cb79
matheus.venera <matheus.venera@dbrsa.com.br>
2022-06-08 11:53:54 -0300

    CRUD de usuários finalizado, com mensagens de sucesso e de erro correspondente para cada método: GET, POST, PUT e DELETE

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,94 @@
 import User from '../models/User';
 
 class UserController {
+  // Adicionar usuário
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Mostrar todos os usuários
+
+  async index(req, res) {
+    try {
+      const listaUsuarios = await User.findAll();
+      return res.json(listaUsuarios);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Mostra somente um usuário procurando pelo ID (Primary Key)
+  async findUserById(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.json('Você precisa passar um id');
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errros: ['Usuário não encontrado'],
+        });
+      }
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async updateUser(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errros: ['Você precisa passar um ID'],
+        });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errros: ['Usuário não cadastrado'],
+        });
+      }
+      const novoUsuario = await user.update(req.body);
+
+      return res.json(novoUsuario);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Deletar usuário pelo ID
+  async deleteUserById(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errros: ['Você precisa passar um ID'],
+        });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errros: ['Usuário não cadastrado'],
+        });
+      }
+
+      await user.destroy();
+      return res.status(200).json('Usuário deletado com sucesso');
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,8 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/criarUsuario', userController.create);
-
+router.get('/listarUsuarios', userController.index);
+router.get('/findUserById/:id', userController.findUserById);
+router.put('/updateUser/:id', userController.updateUser);
+router.delete('/deleteUser/:id', userController.deleteUserById);
 export default router;
